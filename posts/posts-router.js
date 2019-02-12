bf53464a769364ced4397291c38e006ffdf9019c
Andrew Benedict <atbenedict@gmail.com>
2019-02-12 17:14:17 -0500

    All done

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -57,22 +57,18 @@ router.post("/", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   try {
-    const post = await Posts.findById(req.params.id);
-
-    if (post) {
-      try {
-        const count = await Posts.remove(req.params.id);
-        if (count > 0) {
-          res.status(200).json({ message: "The post has been nuked" });
-        }
-      } catch (error) {
-        res
-          .status(404)
-          .json({ message: "The post with the specified ID does not exist" });
-      }
+    const count = await Posts.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: "The post has been removed" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID could not be found" });
     }
   } catch (error) {
-    res.status(500).json({ error: "The post could not be removed" });
+    res.status(500).json({
+      message: "Error removing the post"
+    });
   }
 });
 
